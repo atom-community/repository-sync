52412e03491aab2aeafdefb2de80155de2bd4c00
aminya <aminyahyaabadi74@gmail.com>
2021-07-21 16:34:27 -0500

    feat: add entry file

---
+++ src/main.mjs
@@ -0,0 +1,20 @@
+import { join } from "path"
+import * as git from "./lib/git.mjs"
+import { copyTemplate } from "./lib/files.mjs"
+
+const org = "atom-community"
+const username = "aminya"
+const email = "aminyahyaabadi74@gmail.com"
+
+const cloneFolder = join(process.cwd(), org)
+const templatesFolder = join(process.cwd(), "templates")
+
+async function main() {
+
+  await git.config(username, email)
+
+  const repos = await git.clone(org, cloneFolder)
+
+  await copyTemplate(repos, templatesFolder)
+}
+main()
